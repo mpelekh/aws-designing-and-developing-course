e2eefae5a611a7142d06ffbddeed8edc828eb0aa
Mykola Pelekh <nick.peleh@gmail.com>
2021-08-05 15:43:45 +0300

    Create EC2 instances using AWS SDK

--- scripts/module_02/create-ec2-instance.js
+++ scripts/module_02/create-ec2-instance.js
@@ -1,40 +1,98 @@
 // Imports
-// TODO: Import the aws-sdk
-const helpers = require('./helpers')
+const AWS = require("aws-sdk");
+const helpers = require("./helpers");
 
-// TODO: Configure region
+AWS.config.update({ region: "us-east-1" });
 
 // Declare local variables
-// TODO: Create an ec2 object
-const sgName = 'hamster_sg'
-const keyName = 'hamster_key'
+const ec2 = new AWS.EC2();
+const sgName = "hamster_sg";
+const keyName = "hamster_key";
 
 // Do all the things together
 createSecurityGroup(sgName)
-.then(() => {
-  return createKeyPair(keyName)
-})
-.then(helpers.persistKeyPair)
-.then(() => {
-  return createInstance(sgName, keyName)
-})
-.then((data) => {
-  console.log('Created instance with:', data)
-})
-.catch((err) => {
-  console.error('Failed to create instance with:', err)
-})
+  .then(() => {
+    return createKeyPair(keyName);
+  })
+  .then(helpers.persistKeyPair)
+  .then(() => {
+    return createInstance(sgName, keyName);
+  })
+  .then((data) => {
+    console.log("Created instance with:", data);
+  })
+  .catch((err) => {
+    console.error("Failed to create instance with:", err);
+  });
 
 // Create functions
 
-function createSecurityGroup (sgName) {
-  // TODO: Implement sg creation & setting SSH rule
+function createSecurityGroup(sgName) {
+  const params = {
+    Description: sgName,
+    GroupName: sgName,
+  };
+
+  return new Promise((resolve, reject) => {
+    ec2.createSecurityGroup(params, (err, data) => {
+      if (err) reject(err);
+      else {
+        const params = {
+          GroupId: data.GroupId,
+          IpPermissions: [
+            {
+              IpProtocol: "tcp",
+              FromPort: 22,
+              ToPort: 22,
+              IpRanges: [{ CidrIp: "0.0.0.0/0" }],
+            },
+            {
+              IpProtocol: "tcp",
+              FromPort: 3000,
+              ToPort: 3000,
+              IpRanges: [{ CidrIp: "0.0.0.0/0" }],
+            },
+          ],
+        };
+
+        ec2.authorizeSecurityGroupIngress(params, (err) => {
+          if (err) reject(err);
+          else resolve();
+        });
+      }
+    });
+  });
 }
 
-function createKeyPair (keyName) {
-  // TODO: Create keypair
+function createKeyPair(keyName) {
+  const params = {
+    KeyName: keyName,
+  };
+
+  return new Promise((resolve, reject) => {
+    ec2.createKeyPair(params, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
 }
 
-function createInstance (sgName, keyName) {
-  // TODO: create ec2 instance
+function createInstance(sgName, keyName) {
+  const params = {
+    ImageId: "ami-0c2b8ca1dad447f8a",
+    InstanceType: "t2.micro",
+    KeyName: keyName,
+    MaxCount: 1,
+    MinCount: 1,
+    SecurityGroups: [sgName],
+    UserData:
+      "IyEvYmluL2Jhc2gNCmN1cmwgLS1zaWxlbnQgLS1sb2NhdGlvbiBodHRwczovL3JwbS5ub2Rlc291cmNlLmNvbS9zZXR1cF8xMi54IHwgc3VkbyBiYXNoIC0NCnN1ZG8geXVtIGluc3RhbGwgLXkgbm9kZWpzDQpzdWRvIHl1bSBpbnN0YWxsIC15IGdpdA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9yeWFubXVyYWthbWkvaGJmbC5naXQNCmNkIGhiZmwNCm5wbSBpDQpucG0gcnVuIHN0YXJ0",
+  };
+
+  return new Promise((resolve, reject) => {
+    ec2.runInstances(params, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
 }
