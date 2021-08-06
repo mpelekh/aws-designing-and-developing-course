5ea72417757e6625dcf6f3af26eff5329eef5c31
Mykola Pelekh <nick.peleh@gmail.com>
2021-08-06 12:41:44 +0300

    Create elastic load balancer using AWS SDK

--- scripts/module_03/create-load-balancer.js
+++ scripts/module_03/create-load-balancer.js
@@ -1,71 +1,73 @@
 // Imports
-const AWS = require('aws-sdk')
-const helpers = require('./helpers')
+const AWS = require("aws-sdk");
+const helpers = require("./helpers");
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: "us-east-1" });
 
 // Declare local variables
-// TODO: Create a new ELBv2 object
-const sgName = 'hamsterELBSG'
-const tgName = 'hamsterTG'
-const elbName = 'hamsterELB'
-const vpcId = '/* TODO: Add your VPC Id */'
-const subnets = [
-  /* TODO: Add two subnets */
-]
+const elbv2 = new AWS.ELBv2();
+const sgName = "hamsterELBSG";
+const tgName = "hamsterTG";
+const elbName = "hamsterELB";
+const vpcId = "vpc-1e215563";
+const subnets = ["subnet-a614fcea", "subnet-50e2c70f"];
 
-helpers.createSecurityGroup(sgName, 80)
-.then((sgId) =>
-  Promise.all([
-    createTargetGroup(tgName),
-    createLoadBalancer(elbName, sgId)
-  ])
-)
-.then((results) => {
-  const tgArn = results[0].TargetGroups[0].TargetGroupArn
-  const lbArn = results[1].LoadBalancers[0].LoadBalancerArn
-  console.log('Target Group Name ARN:', tgArn)
-  return createListener(tgArn, lbArn)
-})
-.then((data) => console.log(data))
+helpers
+  .createSecurityGroup(sgName, 80)
+  .then((sgId) =>
+    Promise.all([createTargetGroup(tgName), createLoadBalancer(elbName, sgId)])
+  )
+  .then((results) => {
+    const tgArn = results[0].TargetGroups[0].TargetGroupArn;
+    const lbArn = results[1].LoadBalancers[0].LoadBalancerArn;
+    console.log("Target Group Name ARN:", tgArn);
+    return createListener(tgArn, lbArn);
+  })
+  .then((data) => console.log(data));
 
-function createLoadBalancer (lbName, sgId) {
-  // TODO: Create a load balancer
+function createLoadBalancer(lbName, sgId) {
+  const params = {
+    Name: lbName,
+    Subnets: subnets,
+    SecurityGroups: [sgId],
+  };
+
+  return elbv2.createLoadBalancer(params).promise();
 }
 
-function createTargetGroup (tgName) {
+function createTargetGroup(tgName) {
   const params = {
     Name: tgName,
     Port: 3000,
-    Protocol: 'HTTP',
-    VpcId: vpcId
-  }
+    Protocol: "HTTP",
+    VpcId: vpcId,
+  };
 
   return new Promise((resolve, reject) => {
     elbv2.createTargetGroup(params, (err, data) => {
-      if (err) reject(err)
-      else resolve(data)
-    })
-  })
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
 }
 
-function createListener (tgArn, lbArn) {
+function createListener(tgArn, lbArn) {
   const params = {
     DefaultActions: [
       {
         TargetGroupArn: tgArn,
-        Type: 'forward'
-      }
+        Type: "forward",
+      },
     ],
     LoadBalancerArn: lbArn,
     Port: 80,
-    Protocol: 'HTTP'
-  }
+    Protocol: "HTTP",
+  };
 
   return new Promise((resolve, reject) => {
     elbv2.createListener(params, (err, data) => {
-      if (err) reject(err)
-      else resolve(data)
-    })
-  })
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
 }
