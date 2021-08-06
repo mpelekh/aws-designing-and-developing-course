e4e644ecd439aada202a7c13c99dbe78192cf6f8
Mykola Pelekh <nick.peleh@gmail.com>
2021-08-06 13:15:59 +0300

    Create auto scaling using AWS SDK

--- scripts/module_03/create-auto-scaling.js
+++ scripts/module_03/create-auto-scaling.js
@@ -1,23 +1,46 @@
 // Imports
-const AWS = require('aws-sdk')
+const AWS = require("aws-sdk");
 
-AWS.config.update({ region: '/* TODO: add your region */' })
+AWS.config.update({ region: "us-east-1" });
 
 // Declare local variables
-const autoScaling = new AWS.AutoScaling()
-const asgName = 'hamsterASG'
-const lcName = 'hamsterLC'
-const policyName = 'hamsterPolicy'
-const tgArn = '/* TODO: get target group ARN */'
+const autoScaling = new AWS.AutoScaling();
+const asgName = "hamsterASG";
+const lcName = "hamsterLC";
+const policyName = "hamsterPolicy";
+const tgArn =
+  "arn:aws:elasticloadbalancing:us-east-1:528816537240:targetgroup/hamsterTG/ec2f5d54b1e3d906";
 
 createAutoScalingGroup(asgName, lcName)
-.then(() => createASGPolicy(asgName, policyName))
-.then((data) => console.log(data))
+  .then(() => createASGPolicy(asgName, policyName))
+  .then((data) => console.log(data));
 
-function createAutoScalingGroup (asgName, lcName) {
-  // TODO: Create an auto scaling group
+function createAutoScalingGroup(asgName, lcName) {
+  const params = {
+    AutoScalingGroupName: asgName,
+    AvailabilityZones: ["us-east-1a", "us-east-1b"],
+    TargetGroupARNs: [tgArn],
+    LaunchConfigurationName: lcName,
+    MaxSize: 2,
+    MinSize: 1,
+  };
+
+  return autoScaling.createAutoScalingGroup(params).promise();
 }
 
-function createASGPolicy (asgName, policyName) {
-  // TODO: Create an auto scaling group policy
+function createASGPolicy(asgName, policyName) {
+  const params = {
+    AdjustmentType: "ChangeInCapacity",
+    AutoScalingGroupName: asgName,
+    PolicyName: policyName,
+    PolicyType: "TargetTrackingScaling",
+    TargetTrackingConfiguration: {
+      TargetValue: 5,
+      PredefinedMetricSpecification: {
+        PredefinedMetricType: "ASGAverageCPUUtilization",
+      },
+    },
+  };
+
+  return autoScaling.putScalingPolicy(params).promise();
 }
