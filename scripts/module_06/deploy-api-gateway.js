6b223725e6b243abd959a954f98ff51e430ee0da
Mykola Pelekh <nick.peleh@gmail.com>
2021-08-10 18:45:51 +0300

    Deply API gateway

--- scripts/module_06/deploy-api-gateway.js
+++ scripts/module_06/deploy-api-gateway.js
@@ -1,19 +1,19 @@
 // Imports
-const AWS = require('aws-sdk')
+const AWS = require("aws-sdk");
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: "us-east-1" });
 
 // Declare local variables
-const apiG = new AWS.APIGateway()
-const apiId = '/* TODO: Add api id */'
+const apiG = new AWS.APIGateway();
+const apiId = "pz5t5pkc08";
 
-createDeployment(apiId, 'prod')
-.then(data => console.log(data))
+createDeployment(apiId, "prod").then((data) => console.log(data));
 
-function createDeployment (apiId, stageName) {
-  // TODO: Create params const
+function createDeployment(apiId, stageName) {
+  const params = {
+    restApiId: apiId,
+    stageName,
+  };
 
-  return new Promise((resolve, reject) => {
-    // TODO: Create deployment
-  })
+  return apiG.createDeployment(params).promise();
 }
