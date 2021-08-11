15ffcc18e014c239ecf536111522f607cbe2e900
Mykola Pelekh <nick.peleh@gmail.com>
2021-08-11 14:21:44 +0300

    Create cloud front distribution

--- scripts/module_07/create-cloudfront-distribution.js
+++ scripts/module_07/create-cloudfront-distribution.js
@@ -1,19 +1,27 @@
 // Imports
-const AWS = require('aws-sdk')
-const cfParams = require('./cloudfront-parameters')
+const AWS = require("aws-sdk");
+const cfParams = require("./cloudfront-parameters");
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: "us-east-1" });
 
 // Declare local variables
-// TODO: Create CloudFront SDK Object
+const cf = new AWS.CloudFront();
 
-createDistribution('/* TODO: Add your bucket name */')
-.then(data => console.log(data))
+createDistribution("hamster-bucket-mpelekh").then((data) => console.log(data));
 
-function createDistribution (bucketName) {
-  // TODO: Create params const object
+function createDistribution(bucketName) {
+  const params = {
+    DistributionConfig: {
+      CallerReference: `${Date.now()}`,
+      Comment: "HBFL Distribution",
+      DefaultCacheBehavior: cfParams.defaultCacheBehavior(bucketName),
+      Origins: cfParams.origins(bucketName),
+      HttpVersion: "http2",
+      PriceClass: "PriceClass_100",
+      IsIPV6Enabled: true,
+      Enabled: true,
+    },
+  };
 
-  return new Promise((resolve, reject) => {
-    // TODO: Call createDistribution
-  })
+  return cf.createDistribution(params).promise();
 }
--- util/assets.js
+++ util/assets.js
@@ -1,4 +1,4 @@
-const base = "https://hamster-bucket-mpelekh.s3.amazonaws.com";
+const base = "https://d136jz0qn05bei.cloudfront.net";
 module.exports = {
   hamster1: base + "/images/hamster-1-final.png",
   hamster2: base + "/images/hamster-2-final.png",
