9288ee967a8453e1adc7b21cf06c68c5cf0b5508
Mykola Pelekh <nick.peleh@gmail.com>
2021-08-11 14:29:59 +0300

    Configure S3 website

--- scripts/module_07/configure-s3-website.js
+++ scripts/module_07/configure-s3-website.js
@@ -1,18 +1,22 @@
 // Imports
-const AWS = require('aws-sdk')
+const AWS = require("aws-sdk");
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: "us-east-1" });
 
 // Declare local variables
-const s3 = new AWS.S3()
+const s3 = new AWS.S3();
 
-configureS3Site('/* TODO: Add your S3 bucket name */')
-.then(data => console.log(data))
+configureS3Site("hamster-bucket-mpelekh").then((data) => console.log(data));
 
-function configureS3Site (bucketName) {
-  // TODO: Create params const object
+function configureS3Site(bucketName) {
+  const params = {
+    Bucket: bucketName,
+    WebsiteConfiguration: {
+      IndexDocument: {
+        Suffix: "index.html",
+      },
+    },
+  };
 
-  return new Promise((resolve, reject) => {
-    // Call putBucketWebsite
-  })
+  return s3.putBucketWebsite(params).promise();
 }
